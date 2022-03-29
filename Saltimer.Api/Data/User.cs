1a280605314c90116e9b64540a38abe3e58d362b
Johan Kjellin <johan.kjellin@appliedtechnology.se>
2022-03-29 21:51:13 +0200

    Added new fields Email, createdAt for User Model.

--- Saltimer.Api/Data/User.cs
+++ Saltimer.Api/Data/User.cs
@@ -1,11 +1,20 @@
-﻿namespace Saltimer.Api.Data
+﻿using System.Text.Json.Serialization;
+
+namespace Saltimer.Api.Data
 {
     public class User
     {
+        public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
+        [JsonPropertyName("image_url")]
         public string Url { get; set; } = string.Empty;
+        [JsonPropertyName("email_address")]
+        public string Email { get; set; } = string.Empty;
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        [JsonIgnore]
         public byte[] PasswordHash { get; set; }
+        [JsonIgnore]
         public byte[] PasswordSalt { get; set; }
-
     }
 }
