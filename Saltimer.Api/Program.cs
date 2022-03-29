1470f170dadd21928dee5e3c2e925299e5ed4781
Johan Kjellin <johan.kjellin@appliedtechnology.se>
2022-03-29 21:55:09 +0200

    Added SaltimerDBContext to builder services

--- Saltimer.Api/Program.cs
+++ Saltimer.Api/Program.cs
@@ -4,8 +4,14 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<SaltimerDBContext>(options =>
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SaltimerDBContext")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -42,6 +48,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"));
 
 app.UseHttpsRedirection();
 
